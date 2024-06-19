a0b66f85b31af62a2f90ceb4d0091242a3881a1e
SebasRojasM1 <sebasrojasm1@gmail.com>
2024-06-19 09:26:45 -0500

    The table WORKSPACES  was created with his properties

--- coWorking.sql
+++ coWorking.sql
@@ -8,3 +8,13 @@ CREATE TABLE rooms (
     num_columns INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+/*WORKSPACES*/
+CREATE TABLE workspaces (
+    workspace_id SERIAL PRIMARY KEY,
+    room_id INTEGER NOT NULL,
+    row INTEGER NOT NULL,
+    column INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+);
